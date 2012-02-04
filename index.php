49a9ad20e7264ff95402ae5d4475f68dbf2afdb9
Jordan Little <iamjordanlittle@gmail.com>
2012-02-03 22:52:41 -0600

    Initial commit
    some major changes brewin'
    
    Signed-off-by: Jordan Little <iamjordanlittle@gmail.com>

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Tabler</title>
+	<meta name="description" content="Tabler builds a table with a query you give it." />
+	<link rel="stylesheet" href="themes/default/default.css" />
+</head>
+<body>
+
+
+	<table cellpadding="0" cellspacing="0">
+		<thead>
+			<tr>
+				<th>Name</th>
+				<th>Email</th>
+				<th>Make</th>
+				<th>Model</th>
+				<th>Year</th>
+				<th>Trim</th>
+			</tr>
+		</thead>
+		<tbody>
+			<tr>
+				<td>Jordan Little</td>
+				<td>iamjordanlittle@gmail.com</td>
+				<td>Mazda</td>
+				<td>Miata</td>
+				<td>2000</td>
+				<td>LS</td>
+			</tr>
+			<tr>
+				<td>Brent Coney</td>
+				<td>brentconey@gmail.com</td>
+				<td>Honda</td>
+				<td>Civic</td>
+				<td>2008</td>
+				<td>Mugen Si</td>
+			</tr>
+		</tbody>
+	</table>
+
+
+</body>
+</html>
